cd264bb09400ee853f8139f52b388df524e73246
oteanankaduro <sandro.amofa@ucc.edu.gh>
2025-01-02 16:13:33 +0000

    fleshed out the block.java class

--- src/com/balazsholczer/blockchain/Block.java
+++ src/com/balazsholczer/blockchain/Block.java
@@ -1,4 +1,61 @@
 package com.balazsholczer.blockchain;
 
+import java.util.Date;
+
 public class Block {
+    /*
+    this class is the implementation of the block.
+    * */
+
+    private int id;
+    private int nonce;
+    private long timestamp;
+    private String hash;
+    private String previousHash;
+    private String transaction;
+
+    public Block(int id, String transactions, String previousHash) {
+
+        /*
+        * the contents of the block are placed here.
+        * */
+
+        this.id = id;
+        this.transaction = transaction;
+        this.previousHash = previousHash;
+        this.timestamp = new Date().getTime();
+        generateHash();
+    }
+
+    public void generateHash() {
+
+        String dataToHash = Integer.toString(id) + previousHash + Long.toString(timestamp) + Integer.toString(nonce) + transaction.toString();
+        String hashValue = SHA256Helper.generateHash(dataToHash);
+        this.hash = hashValue;
+    }
+
+    public String getHash() {
+        return this.hash;
+    }
+
+    public void setHash() {
+        this.hash = hash;
+    }
+
+    public String getPreviousHash() {
+        return  this.previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public void incrementNonce() {
+        this.nonce++;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + "-" + this.transaction + "-" + this.hash + "-" + this.previousHash + "-";
+    }
 }
